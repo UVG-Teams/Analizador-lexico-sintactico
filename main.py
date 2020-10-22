d97ecaeafd896c9adfe00c32961ca6f0cd94d306
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-10-21 21:15:17 -0600

    Avances

--- main.py
+++ main.py
@@ -1,113 +1,133 @@
-import ply.lex as lex
-import ply.yacc as yacc
-import networkx as nx
 import matplotlib.pyplot as plt
+import ply.yacc as yacc
+import ply.lex as lex
+import networkx as nx
 
-opcion = 1
+class Analizer(object):
 
-tokens = (
-    'ALPHABET',
-    'LPAREN',
-    'RPAREN',
-    'PREDICATE',
-    'AND',
-    'OR',
-    'SIMPLIES',
-    'IMPLIES',
-    'NEGATION'
-)
+    tokens = (
+        'ALPHABET',
+        'LPAREN',
+        'RPAREN',
+        'PREDICATE',
+        'AND',
+        'OR',
+        'SIMPLIES',
+        'IMPLIES',
+        'NEGATION'
+    )
 
-t_ignore = ' \t'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_AND = r'\^'
-t_OR = r'\°'
-t_IMPLIES = r'\=>'
-t_SIMPLIES = r'\<=>'
-t_NEGATION = r'\~'
+    t_ignore = ' \t'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_AND = r'\^'
+    t_OR = r'\°'
+    t_IMPLIES = r'\=>'
+    t_SIMPLIES = r'\<=>'
+    t_NEGATION = r'\~'
 
-def t_ALPHABET( t ) :
-    r'[p,q,r,s,t,u,v,w,x,y,z]+'
-    if opcion == 1:
-        t.value = bool( t.value )
-    
-    return t
+    precedence = (
+        ( 'left', 'IMPLIES', 'SIMPLIES' ),
+        ( 'left', 'AND', 'OR' ),
+        ( 'left', 'RPAREN', 'LPAREN' ),
+        ( 'right', 'NEGATION' )
+    )
 
-def t_newline( t ):
-  r'\n+'
-  t.lexer.lineno += len( t.value )
+    @staticmethod
+    def t_ALPHABET(t):
+        r'[p,q,r,s,t,u,v,w,x,y,z]+'
+        if option == 1:
+            t.value = bool(t.value)
+        return t
 
-def t_error( t ):
-  print("Invalid Token:",t.value[0])
-  t.lexer.skip( 1 )
+    @staticmethod
+    def t_newline(t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
 
-lexer = lex.lex()
+    @staticmethod
+    def t_error(t):
+        print("Invalid Token:", t.value[0])
+        t.lexer.skip(1)
 
-precedence = (
-    ( 'left', 'IMPLIES', 'SIMPLIES' ),
-    ( 'left', 'AND', 'OR' ),
-    ( 'left', 'RPAREN', 'LPAREN' ),
-    ( 'right', 'NEGATION' )
-)
+    @staticmethod
+    def p_and(p):
+        'expr : expr AND expr'
+        p[0] = p[1] and p[3]
 
-def p_and( p ) :
-    'expr : expr AND expr'
-    p[0] = p[1] and p[3]
+    @staticmethod
+    def p_expr(p):
+        'expr : ALPHABET'
+        p[0] = p[1]
 
-def p_expr( p ) :
-    'expr : ALPHABET'
-    p[0] = p[1] 
+    @staticmethod
+    def p_or(p):
+        'expr : expr OR expr'
+        p[0] = p[1] or p[3]
 
-def p_or( p ) :
-    'expr : expr OR expr'
-    p[0] = p[1] or p[3]
+    @staticmethod
+    def p_negation(p):
+        'expr : NEGATION expr'
+        p[0] = not p[2]
 
-def p_negation( p ) :
-    'expr : NEGATION expr'
-    p[0] = not p[2]
+    @staticmethod
+    def p_implies(p):
+        'expr : expr IMPLIES expr'
+        p[0] = (not p[1]) and p[3]
 
-def p_implies( p ) :
-    'expr : expr IMPLIES expr'
+    @staticmethod
+    def p_simplies(p):
+        'expr : expr SIMPLIES expr'
+        p[0] = not(p[1] ^ p[3])
 
-    p[0] = (not p[1]) and p[3]
+    @staticmethod
+    def p_parens(p):
+        'expr : LPAREN expr RPAREN'
+        p[0] = p[2]
 
-def p_simplies( p ) :
-    'expr : expr SIMPLIES expr'
-    p[0] = not(p[1]^p[3])    #((not p[1]) and p[3]) or ((not p[3]) and p[1]) 
+    @staticmethod
+    def p_error(p):
+        print("Error de sintaxis")
 
-def p_parens( p ) :
-    'expr : LPAREN expr RPAREN'
-    p[0] = p[2]
+    def __init__(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+        self.parser = yacc.yacc(module=self, **kwargs)
 
-def p_error( p ):
-    print("Syntax error in input!")
+    def analize(self, data):
+        self.lexer.input(data)
 
+    def get_value(self, data):
+        return self.parser.parse(data)
 
-continuar = True
-while continuar:
+    def tokenss(self):
+        tokens = []
+        while True:
+            token = self.lexer.token()
+            if not token:
+                break
+            tokens.append(token)
+        return tokens
+
+def menu():
     print("""
         1. Calcular una expresión
         2. Generar grafo
+        9. Salir
     """)
 
-    opcion = int(input("Ingrese una opcion: "))
-    if (opcion == 1):
-        parser = yacc.yacc()
-        res = parser.parse("(p<=>p)")
-        print(int(res))
-
-    elif (opcion == 2):
+option = 1
+analizer = Analizer()
+while True:
+    menu()
+    option = int(input("Ingrese una opcion: "))
+    if (option == 1):
+        expresion = input("Ingrese una expresion a evaluar: ")
+        print("Valor de verdad: ", int(analizer.get_value(expresion)))
+    elif (option == 2):
         G = nx.DiGraph()
-        lexer.input("p<=>~p")
-        tokens = []
-        while True:
-            token = lexer.token()
-            if not token:
-                break
-
-            # print(token)
-            tokens.append(token)
-
+        expresion = input("Ingrese una expresion a graficar: ")
+        analizer.analize(expresion)
+        tokens = analizer.tokenss()
         relations = []
         for i in range(len(tokens)):
             token = tokens[i]
@@ -115,27 +135,39 @@ while continuar:
                 previous_token = tokens[i-1]
             if i+1 < len(tokens):
                 next_token = tokens[i+1]
-            
-            # if token.type == "RPAREN" and i+1 < len(tokens):
-            #     relations.append(("{con}:{label}".format(con = i+1, label = next_token.value), "{con}:{label}".format(con = i-2, label = tokens[i-2].value)))
+
             if token.type == "NEGATION":
                 if next_token.type == "LPAREN":
-                    relations.append(("{con}:{label}".format(con = i, label = token.value), "{con}:{label}".format(con = i+3, label = tokens[i+3].value)))
+                    relations.append((
+                        "{con}:{label}".format(con = i, label = token.value),
+                        "{con}:{label}".format(con = i+3, label = tokens[i+3].value)
+                    ))
                 else:
-                    relations.append(("{con}:{label}".format(con = i, label = token.value), "{con}:{label}".format(con = i+1, label = next_token.value)))
-                    # lexer.input("~(p^q)=>(p°q)")
+                    relations.append((
+                        "{con}:{label}".format(con = i, label = token.value),
+                        "{con}:{label}".format(con = i+1, label = next_token.value)
+                    ))
             elif token.type == "SIMPLIES" or token.type == "IMPLIES" or token.type == "AND" or token.type == "OR":
                 if next_token.type == "LPAREN":
-                    relations.append(("{con}:{label}".format(con = i, label = token.value), "{con}:{label}".format(con = i+3, label = tokens[i+3].value)))
+                    relations.append((
+                        "{con}:{label}".format(con = i, label = token.value),
+                        "{con}:{label}".format(con = i+3, label = tokens[i+3].value)
+                    ))
                 else:
-                    relations.append(("{con}:{label}".format(con = i, label = token.value), "{con}:{label}".format(con = i+1, label = next_token.value)))
+                    relations.append((
+                        "{con}:{label}".format(con = i, label = token.value),
+                        "{con}:{label}".format(con = i+1, label = next_token.value)
+                    ))
                 if previous_token.type == "RPAREN":
-                    relations.append(("{con}:{label}".format(con = i, label = token.value), "{con}:{label}".format(con = i-3, label = tokens[i-3].value)))
+                    relations.append((
+                        "{con}:{label}".format(con = i, label = token.value),
+                        "{con}:{label}".format(con = i-3, label = tokens[i-3].value)
+                    ))
                 else:
-                    relations.append(("{con}:{label}".format(con = i, label = token.value), "{con}:{label}".format(con = i-1, label = previous_token.value)))
-
-            # if token.type != "RPAREN" and token.type != "LPAREN":
-            #     G.add_node("{con}:{label}".format(con = i, label = token.value), value = token.value, label = token.value)
+                    relations.append((
+                        "{con}:{label}".format(con = i, label = token.value),
+                        "{con}:{label}".format(con = i-1, label = previous_token.value)
+                    ))
 
         print(relations)
         G.add_edges_from(relations)
@@ -143,5 +175,7 @@ while continuar:
         nx.draw(G, with_labels=True, font_size="8")
         plt.show()
 
+    elif (option == 9):
+        break
     else:
         print("Opción incorrecta")
